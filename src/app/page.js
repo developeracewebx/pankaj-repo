17459ba7ecc37c88c7db13242f4b410b1ab2f9fd
Pankaj <developer@acewebx.com>
2023-10-05 19:08:30 +0530

    working on streaming

--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,18 @@
 "use client";
 import { useEffect, useRef, useState } from "react";
+import { browserName, CustomView } from "react-device-detect";
 
 export default function Home() {
-  const videoRef = useRef();
+  const safariVideoRef = useRef();
+  const chromeVideoRef = useRef();
+  const firefoxVideoRef = useRef();
   const [loading, setLoading] = useState(true);
 
-  const handlePermissions = async () => {
+  const handlePermissions = async (videoRef) => {
     let permissions = { video: true, audio: true };
     try {
       const stream = await navigator.mediaDevices.getUserMedia(permissions);
-      videoRef.current.srcObject = stream; // Note the change here
+      videoRef.current.srcObject = stream;
       setLoading(false);
       console.log("Camera access granted!");
     } catch (err) {
@@ -18,18 +21,45 @@ export default function Home() {
   };
 
   useEffect(() => {
-    handlePermissions();
+    if (browserName === "Safari") {
+      handlePermissions(safariVideoRef);
+    } else if (browserName === "Chrome") {
+      handlePermissions(chromeVideoRef);
+    } else if (browserName === "Firefox") {
+      handlePermissions(firefoxVideoRef);
+    }
   }, []);
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      {loading && <div>Loading...</div>} {/* Display a loading indicator */}
-      <video
-        ref={videoRef}
-        autoPlay
-        playsInline
-        style={{ display: loading ? "none" : "block" }} // Hide video when loading
-      ></video>
+      {loading && <div>Loading...</div>}
+      <CustomView condition={browserName === "Safari"}>
+        <video
+          ref={safariVideoRef}
+          autoPlay
+          playsInline
+          style={{ display: loading ? "none" : "block" }}
+        ></video>
+        Safari
+      </CustomView>
+      <CustomView condition={browserName === "Chrome"}>
+        <video
+          ref={chromeVideoRef}
+          autoPlay
+          playsInline
+          style={{ display: loading ? "none" : "block" }}
+        ></video>
+        Chrome
+      </CustomView>
+      <CustomView condition={browserName === "Firefox"}>
+        <video
+          ref={firefoxVideoRef}
+          autoPlay
+          playsInline
+          style={{ display: loading ? "none" : "block" }}
+        ></video>
+        Firefox
+      </CustomView>
     </main>
   );
 }
