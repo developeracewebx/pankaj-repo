76b0a549a94914c8e11887fdc6b618d43c3ca28a
Pankaj <developer@acewebx.com>
2023-10-05 21:42:25 +0530

    working on streaming

--- src/app/page.js
+++ src/app/page.js
@@ -9,18 +9,15 @@ export default function Home() {
   const [loading, setLoading] = useState(true);
 
   const handlePermissions = async (videoRef) => {
-    let permissions = { video: true, audio: true };
-    try {
-      const stream = await navigator.mediaDevices.getUserMedia(permissions);
-      videoRef.current.srcObject = stream;
-      setLoading(false);
-      console.log("Camera access granted!");
-    } catch (err) {
-      console.error(err.message);
-    }
-  };
-
-  const handlePermissionsSafari = async (videoRef) => {
+    // let permissions = { video: true, audio: true };
+    // try {
+    //   const stream = await navigator.mediaDevices.getUserMedia(permissions);
+    //   videoRef.current.srcObject = stream;
+    //   setLoading(false);
+    //   console.log("Camera access granted!");
+    // } catch (err) {
+    //   console.error(err.message);
+    // }
     let permissions = { video: true, audio: true };
     try {
       if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
@@ -37,6 +34,63 @@ export default function Home() {
     } catch (err) {
       alert("else path", err);
       console.error(err);
+    }
+  };
+
+  const handlePermissionsSafari = async (videoRef) => {
+    let permissions = { video: true, audio: true };
+    // try {
+    //   if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+    //     const stream = await navigator.mediaDevices.getUserMedia(permissions);
+    //     videoRef.current.srcObject = stream;
+    //     setLoading(false);
+    //     console.log("Camera access granted!");
+    //     alert("if condition work");
+    //   } else {
+    //     // Handle the case where getUserMedia is not supported
+    //     alert(`else part: 'getUserMedia is not supported in this browser.'`);
+    //     console.error("getUserMedia is not supported in this browser.");
+    //   }
+    // } catch (err) {
+    //   alert("else path", err);
+    //   console.error(err);
+    // }
+    if (!navigator.mediaDevices && !navigator.mediaDevices.getUserMedia) {
+      navigator.userMedia = navigator.mozGetUserMedia || navigator.getUserMedia;
+      if (!navigator.userMedia) {
+        alert("Please Update or Use Different Browser");
+        return;
+      }
+      navigator.userMedia(
+        {
+          video: true,
+        },
+        (stream) => showCam(stream),
+        (err) => showErr(err)
+      );
+      return;
+    }
+
+    navigator.mediaDevices
+      .getUserMedia({
+        video: true,
+      })
+      .then((stream) => showCam(stream))
+      .catch((err) => showErr(err));
+
+    function showCam(stream) {
+      let video = document.querySelector("video");
+      videoRef.current.srcObject = stream;
+    }
+
+    function showErr(err) {
+      let message =
+        err.name === "NotFoundError"
+          ? "Please Attach Camera"
+          : err.name === "NotAllowedError"
+          ? "Please Grant Permission to Access Camera"
+          : err;
+      alert(message);
     }
   };
 
